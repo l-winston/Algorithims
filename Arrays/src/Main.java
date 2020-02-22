71e6c775f9f50ab4af824b2f68904a0822118bf7
l-winston <winwinstonliu@gmail.com>
2020-02-21 18:16:33 -0800

    add

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args){
 
     }
 
-
+    int firstDuplicate(int[] a) {
+        HashSet<Integer> seen = new HashSet<Integer>();
+        for(int i = 0; i < a.length; i++){
+            if(seen.contains(a[i]))
+                return a[i];
+            seen.add(a[i]);
+        }
+        return -1;
+    }
 }
