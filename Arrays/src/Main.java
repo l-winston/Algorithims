df0ea970575550ddd07de1468d1de1df5b210ee4
l-winston <winwinstonliu@gmail.com>
2020-02-22 14:55:25 -0800

    add

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -14,4 +14,12 @@ public class Main {
         }
         return -1;
     }
+
+    char firstNotRepeatingCharacter(String s) {
+        for(int i = 0; i < s.length(); i++)
+            if(s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i)))
+                return s.charAt(i);
+        return '_';
+    }
+
 }
