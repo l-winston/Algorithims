e2d69fd46288850b28a828abe37fabb297ce59c7
l-winston <winwinstonliu@gmail.com>
2020-02-28 14:19:39 -0800

    hasPathWithGivenSum

--- Trees/src/Main.java
+++ Trees/src/Main.java
@@ -8,6 +8,15 @@ public class Main {
         Tree<T> right;
     }
 
+    boolean hasPathWithGivenSum(Tree<Integer> t, int s) {
+        if(t == null)
+            return false;
+        if(t.value == s)
+            return true;
+        return hasPathWithGivenSum(t.left, s-t.value) || hasPathWithGivenSum(t.right, s-t.value);
+    }
+
+
     public static void main(String[] args){
 
 
