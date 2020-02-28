8ab306b4168a2669639dc4570f133e046260f7d4
l-winston <winwinstonliu@gmail.com>
2020-02-28 14:15:51 -0800

    added tree class

--- Trees/src/Main.java
+++ Trees/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-
+    class Tree<T> {
+        Tree(T x) {
+            value = x;
+        }
+        T value;
+        Tree<T> left;
+        Tree<T> right;
+    }
 
     public static void main(String[] args){
 
