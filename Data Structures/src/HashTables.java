abf1fe6903054ad9bef00604aaf5fc31ca3e4bc3
l-winston <winwinstonliu@gmail.com>
2020-03-21 12:51:21 -0700

    solved groupingDishes

--- Data Structures/src/HashTables.java
+++ Data Structures/src/HashTables.java
@@ -1,6 +1,42 @@
 import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class HashTables {
+
+    String[][] groupingDishes(String[][] dishes) {
+        TreeMap<String, TreeSet<String>> map = new TreeMap<>();
+        TreeSet<String> pass = new TreeSet<>();
+        for(int i = 0; i < dishes.length; i++){
+            String dish = dishes[i][0];
+            for(int j = 1; j < dishes[i].length; j++){
+                String ingred = dishes[i][j];
+                if(!map.keySet().contains(ingred)){
+                    TreeSet<String> set = new TreeSet<String>();
+                    set.add(dish);
+                    map.put(ingred, set);
+                }else{
+                    map.get(ingred).add(dish);
+                    pass.add(ingred);
+                }
+            }
+        }
+
+        String[][] ret = new String[pass.size()][];
+
+        int i = 0;
+        for(String s : pass){
+            ret[i] = new String[map.get(s).size() + 1];
+            ret[i][0] = s;
+            int j = 1;
+            for(String ss : map.get(s)){
+                ret[i][j++] = ss;
+            }
+            i++;
+        }
+
+        return ret;
+    }
 
     boolean areFollowingPatterns(String[] strings, String[] patterns) {
         HashMap<String, String> fwd = new HashMap<String, String>();
