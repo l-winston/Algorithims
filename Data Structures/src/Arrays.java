4ba8220c471cb3b14898ee5cc301d1eb9eaa6962
l-winston <winwinstonliu@gmail.com>
2020-03-30 12:20:00 -0700

    solution for rotateImage

--- Data Structures/src/Arrays.java
+++ Data Structures/src/Arrays.java
@@ -19,6 +19,16 @@ public class Arrays {
         return '_';
     }
 
+    int[][] rotateImage(int[][] a) {
+        int[][] r = new int[a[0].length][a.length];
+
+        for(int i = 0; i < a.length; i++)
+            for(int j = 0; j < a[0].length; j++)
+                r[i][j] = a[a[0].length-j-1][i];
+
+        return r;
+    }
+
     public static void main(String[] args){
         Arrays a = new Arrays();
 
